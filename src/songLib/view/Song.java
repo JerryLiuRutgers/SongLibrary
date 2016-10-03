9b0cfac01032b34968032c47625bf92e91af78b4
JerryLiuRutgers <jerry.liu@rutgers.edu>
2016-10-03 03:16:55 -0400

    Added Song class, began implementing song list creation from text file

---
+++ src/songLib/view/Song.java
@@ -0,0 +1,72 @@
+package songLib.view;
+
+public class Song {
+	
+	private String name;
+	private String artist;
+	private String album;
+	private String year;
+	
+	//Constructors
+	public Song(String name, String artist) {
+		this.name = name;
+		this.artist = artist;
+		album = "";
+		year = "";
+	}
+	
+	public Song(String name, String artist, String fieldThree, int x) {
+		this.name = name;
+		this.artist = artist;
+		
+		if (x == 0) {
+			album = fieldThree;
+			year = "";
+		}
+		else {
+			year = fieldThree;
+			album = "";
+		}
+	}
+	
+	public Song(String name, String artist, String album, String year) {
+		this.name = name;
+		this.artist = artist;
+		this.album = album;
+		this.year = year;
+	}
+	
+	//Getters
+	public String getName() {
+		return name;
+	}
+	
+	public String getArtist() {
+		return artist;
+	}
+	
+	public String getAlbum() {
+		return album;
+	}
+	
+	public String getYear() {
+		return year;
+	}
+	
+	//Setters
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void setArtist(String artist) {
+		this.artist = artist;
+	}
+	
+	public void setAlbum(String album) {
+		this.album = album;
+	}
+	
+	public void setYear(String year) {
+		this.year = year;
+	}
+}
--- src/songLib/view/SongLibController.java
+++ src/songLib/view/SongLibController.java
@@ -1,5 +1,10 @@
 package songLib.view;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Optional;
 
 import javafx.collections.ObservableList;
@@ -10,11 +15,24 @@ import javafx.stage.Stage;
 public class SongLibController {
 	@FXML
 	ListView<String> listView;
+	ArrayList<Song> list;
 	
 	private ObservableList<String> obsList;
 	
 	public void start(Stage mainStage) {
 		
 	}
-
+	
+	// Each song should be stored in songs.txt in the form:
+	// title|artist|album|year
+	private void readList() throws IOException {
+		String line;
+		
+		FileReader fileReader = new FileReader("/songLib/view/songs.txt");
+		BufferedReader bufferedReader = new BufferedReader(fileReader);
+		
+		while((line = bufferedReader.readLine()) != null) {
+			
+		}
+	}
 }
